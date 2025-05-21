66eb6bdd02b5334f65ef59e168dc9d542dce81a1
Arctic <wzrkey@gmail.com>
2025-05-21 02:15:55 -0700

    add a new anaylyze email function

--- backend/app/routes/scan.py
+++ backend/app/routes/scan.py
@@ -7,7 +7,9 @@ bp = Blueprint('scan', __name__, url_prefix='/scan')
 def list():
     try:
         if session['authenticated'] == True:
+            # return a list of custom email details in json format
             return list_inbox_messages_most_recent(current_app.gmail_service)
     except Exception as e:
         print("error1")
         return jsonify(error=str(e)), 500
+
